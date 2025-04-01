5ed2e26c50370966eb480c8e5a53338e20a80b72
yimam69 <130046579+yimam69@users.noreply.github.com>
2025-04-01 22:38:43 +1100

    Move to string vector (Partial)

--- Force_Generation.m
+++ Force_Generation.m
@@ -3,6 +3,10 @@ function [Forces, NumNodes,TimeSteps, NumTimeSteps] = Force_Generation(Inputfile
 
 %Read data into matrix
 RawData = readmatrix(Inputfilename);
+
+%Read data into string array
+RawDataString = readlines(Inputfilename);
+
 %Read data into cell array
 %RawDataCell = fileread(Inputfilename):
 
@@ -60,13 +64,19 @@ i3 = 0;
 %%Loop to fill in new array
 for(i = 1:NumTimeSteps);
     for(i2 = 1:NumNodes);
-    
-        j2 = i2*2+(i-1)*3+(i-1)*NumNodes*2+1;
+        if i == 1 & i2 == 1
+            j2 = 17
+        elseif i2 == 1
+            j2 = j2+9;
+        else
+            j2 = j2+2;
+        end
         
         k2 = j2-1;
         
         %Fill in node number
-        Forces(i2,1,i) = RawData(k2,1);
+        splitStr = regexp(RawDataString(k2),'         ','split');
+        Forces(i2,1,i) = splitStr(1);
 
         %Statement used to plot individual nodes
         if  RawData(k2,1) == 2360;
@@ -80,15 +90,25 @@ for(i = 1:NumTimeSteps);
             %Fill in z force
             plotz(i3) = RawData(j2,3);
         end
-            
+
+        %Extract values from String array
+        newStr = regexprep(RawDataString(j2),'                  ','');
+        splitStr = regexp(newStr,'  ','split');
+        xforce = str2num(splitStr(1));
+        
+        yforce = str2num(splitStr(2));
+
+        zforce = str2num(splitStr(3));
+
         %Fill in x force
-        Forces(i2,2,i) = RawData(j2,1);
+        Forces(i2,2,i) = xforce;
 
         %Fill in y force
-        Forces(i2,3,i) = RawData(j2,2);
+        Forces(i2,3,i) = yforce;
 
         %Fill in z force
-        Forces(i2,4,i) = RawData(j2,3);
+        Forces(i2,4,i) = zforce;
+       
     end
 end
 %Save forces to output
