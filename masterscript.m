4df9573564bc59b4a44855d03e0d9e89fcfdeb07
yimam69 <130046579+yimam69@users.noreply.github.com>
2025-04-19 09:39:34 +1000

    Added node force plot functionality
    Added node force plot functionality

--- masterscript.m
+++ masterscript.m
@@ -1,11 +1,16 @@
+clc
 %%Master_Script
 %%NOTE: ENSURE modified file HAS BEEN DELETED FROM directory BEFORE RUNNING
 Inputfilename = 'ncforc';
 KFileName = 'Model_2.k';
 
+%For use to create force plot for particular node
 
-%Run force generation script
-[Forces, NumNodes, TimeSteps, NumTimeSteps] = Force_Generation(Inputfilename);
+PlotFlag = 1; %1 to turn on node plot, 0 to turn off node plot
+PlotNode = 2360; % Node to Plot
 
-%Run kfile generator
+%%Run force generation script
+[Forces, NumNodes, TimeSteps, NumTimeSteps] = Force_Generation(Inputfilename, PlotFlag, PlotNode);
+
+%%Run kfile generator
 output = Keyword_Writing(Forces, NumNodes,TimeSteps, NumTimeSteps, KFileName);
