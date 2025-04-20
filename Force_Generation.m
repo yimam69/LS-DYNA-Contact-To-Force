d23949d1490c52f69a03eba2145db9028dfcf8da
yimam69 <130046579+yimam69@users.noreply.github.com>
2025-04-21 08:22:55 +1000

    Fixed to use readlines again. didn't save a bunch of changes last time

--- Force_Generation.m
+++ Force_Generation.m
@@ -1,81 +1,101 @@
 %% Result File Reader
-function [Forces, NumNodes,TimeSteps, NumTimeSteps] = Force_Generation(Inputfilename)
+function [Forces, NumNodes,TimeSteps, NumTimeSteps] = Force_Generation(Inputfilename, PlotFlag, PlotNode)
 
 %Read data into matrix
-RawData = readmatrix(Inputfilename);
+text = readlines(Inputfilename);
 %Read data into cell array
 %RawDataCell = fileread(Inputfilename):
 
 %Find locations of NaN values
-LocationIndex = isnan(RawData);
-LocationIndex = LocationIndex(:,1);
+LocationIndex = ismissing(text,'');
 IndexLength = length(LocationIndex);
-
-%Create index with locations of each timestep
-j = 1;
-
-for (i=1:IndexLength)
-    if(LocationIndex(i) == 1 & LocationIndex(i+1) == 1 & LocationIndex(i+2) == 1)
-        
-        LocationIndex1(j) = i+2;
-        j = j+1;
-        
-    end
-end
+LocationIndex1 = find(LocationIndex);
 
 %Read timesteps
-NumTimeSteps = length(LocationIndex1);
+NumTimeSteps = (length(LocationIndex1)-3)/4;
 
 %Preallocate timestep array
 TimeSteps = NaN(NumTimeSteps,1);
 
-text = readlines(Inputfilename);
-
+i2 = 1;
+%Fill in timesteps vector
 for (i = 1:NumTimeSteps);
+    
+    i2 = i2 + 4;
 
-    j = LocationIndex1(i);
-
-    if(i == 1)
-        j = 12;
-    else
-      
-        j = j + 12 + 4*(i-1);
-    end
+    j = LocationIndex1(i2)+1;
 
     Textfiltered = text(j);
     TextFilterNum = regexprep(Textfiltered,'forces (t=','');
     TextFilterNum = regexprep(TextFilterNum,') for interface         2 surfa  side','');
     TimeSteps(i) = str2num(TextFilterNum);
 
-
 end
 
 %Determine number of nodes
-NumNodes = length(RawData);
-NumNodes = ceil(NumNodes/(2*NumTimeSteps))-2;
+NumNodes = length(text);
+NumNodes = ceil((NumNodes-15-(7*NumTimeSteps))/(2*NumTimeSteps));
 
 %Preallocate forces array
 Forces = NaN(NumNodes,4,NumTimeSteps);
 
+%Stateent to preallocate plot array
+if PlotFlag == 1
+             
+            plotx = NaN(NumTimeSteps);
+
+            
+            ploty = NaN(NumTimeSteps);
+
+            
+            plotz = NaN(NumTimeSteps);
+            i3 = 0;
+end
+
 %%Loop to fill in new array
+
 for(i = 1:NumTimeSteps);
     for(i2 = 1:NumNodes);
-    
-        j2 = i2*2+(i-1)*3+(i-1)*NumNodes*2;
-        
-        k2 = j2-1;
+        if i == 1 & i2 == 1
+            
+        j2 = 16;
+
+        elseif i2 == 1
+            j2 = j2+9;
+        else
+            j2 = j2+2;
+        end
+
         
         %Fill in node number
-        Forces(i2,1,i) = RawData(k2,1);
+        splitStr = regexp(text(j2),'        ','split');
+        Forces(i2,1,i) = splitStr(1);
 
-        %Fill in x force
-        Forces(i2,2,i) = RawData(j2,1);
+        %Extract values from String array
+        newStr = regexprep(text(j2),'      ....        ','');
+        splitStr = regexp(newStr,'  ','split');
+        xforce = str2num(splitStr(1));
+        Forces(i2,2,i) = xforce;
 
-        %Fill in y force
-        Forces(i2,3,i) = RawData(j2,2);
+        yforce = str2num(splitStr(2));
+        Forces(i2,3,i) = yforce;
 
-        %Fill in z force
-        Forces(i2,4,i) = RawData(j2,3);
+        zforce = str2num(splitStr(3));
+        Forces(i2,4,i) = zforce;
+        
+        %Statement used to plot individual nodes
+        if  Forces(i2,1,i) == PlotNode && PlotFlag == 1
+            i3 = i3+1;
+             %Fill in x force
+            plotx(i3) = xforce;
+
+            %Fill in y force
+            ploty(i3) = yforce;
+
+            %Fill in z force
+            plotz(i3) = zforce;
+        end
+
     end
 end
 %Save forces to output
